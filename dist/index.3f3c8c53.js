8e792df2408e7c7290755312d227b1b97a6ccbbb
biswajitpatra <biswajit1305@gmail.com>
2024-07-12 09:46:57 +0530

    Updated paths and structure

--- dist/index.3f3c8c53.js
+++
@@ -1,2 +0,0 @@
-!function(){let e=document.querySelector("#no_of_years");if(e){var n=moment(new Date(2016,12,1));let t=moment(new Date).diff(n,"years",!0).toFixed(1);e.innerHTML="<b>"+t+" years</b>"}}();
-//# sourceMappingURL=index.3f3c8c53.js.map
